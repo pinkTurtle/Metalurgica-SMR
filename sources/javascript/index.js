b645db7ed70890411953898d5f14fff801106b6c
Juan Facundo Suarez <facundo@svarez.com.ar>
2013-10-21 19:21:04 -0300

    iniciando codigo para implementar slider

---
+++ sources/javascript/index.js
@@ -0,0 +1,6 @@
+/**
+ * Module dependencies
+ */
+
+var o = jQuery = require('jquery');
+
---
+++ website/wp-content/themes/smr/front-page.php
@@ -0,0 +1,25 @@
+<?php
+get_header(); ?>
+
+  <div id="home-primary" class="home-content-area">
+    <div id="home-content" class="front-content" role="main">
+
+      <div class="my-slider">
+        <ul>
+          <li>
+          <img src='<?php echo get_bloginfo('template_url'); ?>/slide01.jpg' />
+          </li>
+          <li>
+          <img src='<?php echo get_bloginfo('template_url'); ?>/slide02.jpg' />
+          </li>
+          <li>
+          <img src='<?php echo get_bloginfo('template_url'); ?>/slide01.jpg' />
+          </li>
+          <li>
+          <img src='<?php echo get_bloginfo('template_url'); ?>/slide02.jpg' />
+          </li>
+        </ul>
+      </div>
+
+    </div><!-- #content -->
+  </div><!-- #primary -->
