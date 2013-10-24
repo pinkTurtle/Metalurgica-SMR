59885a8c1612800a7024d342262823d883f435f4
Juan Facundo Suarez <facundo@svarez.com.ar>
2013-10-24 18:07:00 -0300

    agregando build.styl

--- website/wp-content/themes/smr/front-page.php
+++ website/wp-content/themes/smr/front-page.php
@@ -1,44 +1,43 @@
 <?php
 get_header(); ?>
 
+<div class="my-slider">
+  <ul id="slides">
+    <?php
+    $initslides = new WP_Query(array(
+      'post_type' => 'imgslides',
+      'orderby' => 'rand',
+      'showposts' => 5,
+      'post_status'=>'publish'
+    ));
+    // The Loop
+    if (have_posts()) :
+      while ($initslides->have_posts()) :
+      $initslides->the_post();
+      $post_thumb_id = get_post_thumbnail_id($post->ID);
+      $img = wp_get_attachment_image_src($post_thumb_id, 'single-post-thumbnail');
+      $url = $img[0];?>
 
-      <div class="my-slider">
-        <ul id="slides">
-          <?php
-          $initslides = new WP_Query(array(
-            'post_type' => 'imgslides',
-            'orderby' => 'rand',
-            'showposts' => 5,
-            'post_status'=>'publish'
-          ));
-          // The Loop
-          if (have_posts()) :
-            while ($initslides->have_posts()) :
-            $initslides->the_post();
-            $post_thumb_id = get_post_thumbnail_id($post->ID);
-            $img = wp_get_attachment_image_src($post_thumb_id, 'single-post-thumbnail');
-            $url = $img[0];?>
+      <li>
+        <div class="desc">
+          <p><?php the_title(); ?></p>
+          <?php //the_content(); ?>
+        </div>
+        <img src="<?php echo $url; ?>" alt="SMR"/>
+      </li>
 
-            <li>
-              <div class="desc">
-                <p><?php the_title(); ?></p>
-                <?php //the_content(); ?>
-              </div>
-              <img src="<?php echo $url; ?>" alt="SMR"/>
-            </li>
+      <?php
+      endwhile;
+    endif;
+    // end loop
+    wp_reset_postdata();
+    ?><!--fin init slides aleatorios -->
+  </ul><!-- fin slides -->
+</div><!-- fin my-slider -->
 
-            <?php
-            endwhile;
-          endif;
-          // end loop
-          wp_reset_postdata();
-          ?><!--fin init slides aleatorios -->
-        </ul><!-- fin slides -->
-      </div><!-- fin my-slider -->
+<main id="frontpage primary" class="home-content-area">
+  <div id="frontpage content" class="front-content" role="main">
 
-  <main id="frontpage primary" class="home-content-area">
-    <div id="home-content" class="front-content" role="main">
-
-    </div><!-- #content -->
-  </main><!-- #primary -->
+  </div><!-- #content -->
+</main><!-- #primary -->
 <?php get_footer(); ?>
