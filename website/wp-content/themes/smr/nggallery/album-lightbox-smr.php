bf7cc2df756e743ffc627a3c463ecaca3f03bbde
Juan Facundo Suarez <facundo@svarez.com.ar>
2013-11-05 20:09:22 -0300

    ajustando albumes y pretty foto

---
+++ website/wp-content/themes/smr/nggallery/album-lightbox-smr.php
@@ -0,0 +1,52 @@
+<?php 
+/**
+Template Page for the album overview
+
+Follow variables are useable :
+
+  $album       : Contain information about the album
+  $galleries   : Contain all galleries inside this album
+  $pagination  : Contain the pagination content
+
+ You can check the content when you insert the tag <?php var_dump($variable) ?>
+ If you would like to show the timestamp of the image ,you can use <?php echo $exif['created_timestamp'] ?>
+**/
+?>
+<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($galleries)) : ?>
+
+<div class="my-slider smr-slider ngg-albumoverview">   
+  <div>
+
+    <!-- List of galleries -->
+    <?php foreach ($galleries as $gallery) : ?>
+    
+    <div class="smr-album">
+        <div class="ngg-album-link"  id="<?php echo $gallery->name ?>">
+          <?php $images =  nggGetGallery( $gallery->gid ); 
+          $imgcntr = 0;
+          ?>
+
+          <?php foreach ( $images as $image ) : ?>
+            <?php if ( $imgcntr == 0 ): ?>
+              <a href="<?php echo $image->imageURL ?>" title="<?php echo str_replace("\"", "'", $image->description); ?>" <?php echo $image->thumbcode ?> >
+                <img class="Thumb" alt="<?php echo $gallery->title ?>" src="<?php echo $gallery->previewurl ?>"/>
+                <span><?php echo $gallery->title ?></span>
+              </a>
+              <?php $imgcntr++; ?>
+            <?php else: ?>
+              <a href="<?php echo $image->imageURL ?>" title="<?php echo str_replace("\"", "'", $image->description); ?>" <?php echo $image->thumbcode ?> ></a>
+            <?php endif; ?>
+          <?php endforeach; ?>
+        </div>
+    </div>
+
+    <?php endforeach; ?>
+    
+    <!-- Pagination -->
+    <?php echo $pagination ?>
+
+  </div>
+</div>
+
+<?php endif; ?>
+
---
+++ website/wp-content/themes/smr/nggallery/album-smr.php
@@ -0,0 +1,39 @@
+<?php
+/**
+Template Page for the album overview
+
+Follow variables are useable :
+
+	$album     	 : Contain information about the album
+	$galleries   : Contain all galleries inside this album
+	$pagination  : Contain the pagination content
+
+ You can check the content when you insert the tag <?php var_dump($variable) ?>
+ If you would like to show the timestamp of the image ,you can use <?php echo $exif['created_timestamp'] ?>
+
+   Script modificado para hacer el album smr
+**/
+?>
+<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($galleries)) : ?>
+
+<div class="my-slider smr-slider ngg-albumoverview">
+  <div>
+
+    <!-- List of galleries -->
+    <?php foreach ($galleries as $gallery) : ?>
+
+    <div class="smr-album">
+      <a class="Link" href="<?php echo $gallery->pagelink ?>">
+        <img class="Thumb" alt="<?php echo $gallery->title ?>" src="<?php echo $gallery->previewurl ?>"/>
+      </a>
+    </div>
+
+    <?php endforeach; ?>
+
+  </div>
+  <!-- Pagination -->
+    <br class="ngg-clear"/>
+  <?php echo $pagination ?>
+</div>
+
+<?php endif; ?>
