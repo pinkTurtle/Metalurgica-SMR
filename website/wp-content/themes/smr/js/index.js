89e153b32b9e9f20fc81ee29058cfff05ec7c2f3
Juan Facundo Suarez <facundo@svarez.com.ar>
2013-11-18 15:08:46 -0300

    ajustando js

--- website/wp-content/themes/smr/js/index.js
+++ website/wp-content/themes/smr/js/index.js
@@ -38,7 +38,43 @@ o(document).ready(function() {
     }, 7000);
   }
 
+  // conent slider
+  if (o('.content-slider').length) {
+    debug('Rendering main slider');
+
+    var NewSlider = require('newslider');
+    var firstslide = o('a.firstslide');
+    var nextslide = o('a.nextslide');
+    var prevslide = o('a.prevslide');
+    var lastslide = o('a.lastslide');
+
+    var mySlider = new NewSlider('.content-slider');
+
+    lastslide.click(function(){
+      mySlider.last();
+    });
+
+    firstslide.click(function(){
+      mySlider.first();
+    });
+
+    nextslide.click(function(){
+      mySlider.next();
+    });
+
+    prevslide.click(function(){
+      mySlider.prev();
+    });
+
+    var slideTimer = window.setInterval(function(){
+      mySlider.next();
+    }, 7000);
+  }
+
+
+
     o(".descripciones .servicios").click(function() {
+      if (o(this).find('.contenido-servicios').css('display')=='none') {
         o('.descripciones .servicios .contenido-servicios').slideUp( "slow" );
         o('.contenido-padre').slideUp( "slow" );
         o('.contenido-padre').css( 'opacity', '0' );
@@ -53,11 +89,15 @@ o(document).ready(function() {
         contentSlider = null;
         clearInterval(slideTimer);
 
+      if (o(this).find('.content-slider').length) {
         NewSlider = require('newslider');
         contentSlider = new NewSlider(o(this).find('.content-slider'));
+
         slideTimer = window.setInterval(function(){
           contentSlider.next();
         }, 7000);
+      }
+      }
 
     });
 
